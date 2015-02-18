88c3915e7d7008217a43f6eb90c657d41e9a6061
domspad <domspad@umich.edu>
2015-02-17 19:52:51 -0800

    an unclear question, but good to know ans

---
+++ q3_6.py
@@ -0,0 +1,22 @@
+
+def sortWithStack(stack, reverse=False) :
+	"sorts stack (increasing) using another stack"
+	tmpstack = []
+	while(stack != []) :
+		tmp = stack.pop()
+		while(tmpstack != [] and ((tmpstack[-1] < tmp) != reverse)) :
+			stack.append(tmpstack.pop())
+		tmpstack.append(tmp)
+	while(tmpstack != []) :
+		stack.append(tmpstack.pop())
+
+def test_sort() :
+	ex = [0,5,4,2,1,3]
+	sortWithStack(ex)
+	assert ex == range(6)
+	sortWithStack(ex,True)
+	assert ex == range(5,-1,-1)
+	print "passes tests"
+
+if __name__ == '__main__' :
+	test_sort()
