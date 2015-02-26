cbc00a967f867451898ad7631eea162d7dfdec23
domspad <domspad@umich.edu>
2015-02-26 10:53:53 -0800

    cleaning up solutions

--- q1_1.py
+++ q1_1.py
@@ -1,8 +1,7 @@
-
-
+"""
 # Chapter 1: Arrays and Strings
 # Problem 1: 
-"""
+
 Implement an algorithm to determine if a string has all unique characters. What
 if you cannot use additional data structures?
 """
--- q1_2.py
+++ q1_2.py
@@ -1,8 +1,29 @@
+"""
+# Chapter 1: Arrays and Strings
+# Problem 2: 
 
+Write code to reverse a C-Style String (e.g. 'abcd' == ['a','b','c','d',''])
+"""
+
+################################################################################
+################################################################################
+################################################################################
+# SOLUTIONS
+
+"""
+Solution 1 : 
+
+Finds end of string then marches backwards appending letters to a new string
+
+Time: 		O(n)
+Space:		O(n)
+"""
 
 def reverseCString1(cstr) :
-	# not in place
-	""" ex: cstr == ['h', 'e', 'l','l','o',''], and len() is not O(1) """
+	""" 
+	Requires: cstr is a sequence of characters where one character is ''
+	Returns: list with characters of cstr reversed (last char is '')
+	"""
 	length = 0
 	while(cstr[length] != '' ) :
 		length += 1
@@ -12,34 +33,17 @@ def reverseCString1(cstr) :
 	result.append('')
 	return result
 
-# def reverseCString2(cstr) :
-# 	""" ex: cstr == ['h', 'e', 'l','l','o',''], and len() is not O(1) """
-# 	# in place?
-# 	length = 0
-# 	while(cstr[length] != '' ) :
-# 		length += 1
-# 	result = []
-# 	for i in range(length) :
-# 		result.append(cstr[length - i])
-# 	return result
+"""
+Solution 2 : 
 
+Iterate through chars of string until end 
+Then with two end pointers, swap chars of string until both pointers reach middle
 
-def test_reverse() :
-	ex = [([''],['']),
-			(['a',''],['a','']),
-			(['a','b',''],['b','a',''])]
-	assert all(reverseCString1(cstr[0]) == cstr[1] for cstr in ex)
-	print 'passes tests'
+Time: 		O(n)
+Space:		O(1)
+"""
 
-if __name__ == '__main__' :
-	test_reverse()
-
-#incorporate
-#############################
-#Divide
-#############################
-
-def reverseCString(cstr) :
+def reverseCString2(cstr) :
 	start = 0
 	end = 0
 	# find end
@@ -53,14 +57,27 @@ def reverseCString(cstr) :
 		start += 1
 		end -= 1
 
-def test_reverse() :
+################################################################################
+################################################################################
+################################################################################
+# TESTS
+
+def test_reverse1() :
+	ex = [([''],['']),
+			(['a',''],['a','']),
+			(['a','b',''],['b','a',''])]
+	assert all(reverseCString1(cstr[0]) == cstr[1] for cstr in ex)
+	print 'passes tests'
+
+def test_reverse2() :
 	exs = [([''],['']),
 			(['a',''],['a','']),
 			(['a','b',''],['b','a',''])]
 	for ex in exs :
-		reverseCString(ex[0])
+		reverseCString2(ex[0])
 		assert ex[0] == ex[1] 
 	print 'passes tests'
 
 if __name__ == '__main__' :
-	test_reverse()
+	test_reverse1()
+	test_reverse2()
